55cefa026241cc7d0bccb54f7464df883adb96d9
Benjamin Marwell <bmarwell@apache.org>
2021-12-29 13:10:26 +0100

    init linuxcounter

---
+++ web/rest-v1/src/main/java/io/github/linuxcounter/web/rest/v1/LiCoApplication.java
@@ -0,0 +1,8 @@
+package io.github.linuxcounter.web.rest.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class LiCoApplication extends Application {
+}
---
+++ web/rest-v1/src/main/java/io/github/linuxcounter/web/rest/v1/resources/CounterResource.java
@@ -0,0 +1,25 @@
+package io.github.linuxcounter.web.rest.v1.resources;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.apache.shiro.authz.annotation.RequiresUser;
+
+@Path("/count")
+public class CounterResource {
+
+  @POST
+  @RequiresUser
+  public Response postCount() {
+    return Response.status(Status.NOT_IMPLEMENTED)
+        .build();
+  }
+
+  @POST
+  @Path("/anon")
+  public Response postCountAnonymous() {
+    return Response.status(Status.NOT_IMPLEMENTED)
+        .build();
+  }
+}
