1c89dd4710ff35c0b9d12a72ffdafaa24cacb5d8
Benjamin Marwell <bmarwell@apache.org>
2021-12-30 08:47:06 +0100

    configure shiro for JAX-RS.

--- web/rest-v1/src/main/java/io/github/linuxcounter/web/rest/v1/resources/CounterResource.java
+++ web/rest-v1/src/main/java/io/github/linuxcounter/web/rest/v1/resources/CounterResource.java
@@ -1,5 +1,6 @@
 package io.github.linuxcounter.web.rest.v1.resources;
 
+import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -7,6 +8,7 @@ import javax.ws.rs.core.Response.Status;
 import org.apache.shiro.authz.annotation.RequiresUser;
 
 @Path("/count")
+@RequestScoped
 public class CounterResource {
 
   @POST
