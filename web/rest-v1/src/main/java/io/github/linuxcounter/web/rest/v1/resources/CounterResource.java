58b2ad033d55d8201addeb9b4c95f5c7aecf0131
Benjamin Marwell <bmarwell@apache.org>
2021-12-30 18:01:21 +0100

    add basic OpenAPI metadata.

--- web/rest-v1/src/main/java/io/github/linuxcounter/web/rest/v1/resources/CounterResource.java
+++ web/rest-v1/src/main/java/io/github/linuxcounter/web/rest/v1/resources/CounterResource.java
@@ -2,26 +2,52 @@ package io.github.linuxcounter.web.rest.v1.resources;
 
 import io.github.linuxcounter.common.value.ReportData;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import org.apache.shiro.authz.annotation.RequiresUser;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/count")
 @RequestScoped
+@Tag(name = "counter")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
 public class CounterResource {
 
   @POST
   @RequiresUser
-  public Response postCount(ReportData reportData) {
+  public Response postCount(
+      @RequestBody(
+          required = true,
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(required = true, ref = "#/components/schemas/ReportData")
+          )
+      )
+          ReportData reportData) {
     return Response.status(Status.NOT_IMPLEMENTED)
         .build();
   }
 
   @POST
   @Path("/anon")
-  public Response postCountAnonymous(ReportData reportData) {
+  public Response postCountAnonymous(
+      @RequestBody(
+          required = true,
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(required = true, ref = "#/components/schemas/ReportData")
+          )
+      )
+          ReportData reportData) {
     return Response.status(Status.NOT_IMPLEMENTED)
         .build();
   }
