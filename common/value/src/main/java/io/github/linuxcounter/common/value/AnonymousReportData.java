ae26f68fd3193ba4b6785075d3b829c7dd664e7a
Benjamin Marwell <bmarwell@apache.org>
2022-01-01 23:01:16 +0100

    RegisteredReportData vs AnonymousReportData // OpenAPI 3 // surefire version

--- common/value/src/main/java/io/github/linuxcounter/common/value/AnonymousReportData.java
+++ common/value/src/main/java/io/github/linuxcounter/common/value/AnonymousReportData.java
@@ -3,9 +3,7 @@ package io.github.linuxcounter.common.value;
 /**
  * Incoming report data, e.g. when a registered or anonymous user sends data.
  */
-public record ReportData(
-    UserId userId,
-    SystemId systemId,
+public record AnonymousReportData(
     String kernelName,
     String kernelVersion,
     String distributionName,
